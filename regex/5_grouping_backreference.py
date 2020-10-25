bebefb1efd83be3fd82453fc9d6dc271433fe483
thedoodler <ramprakashsmail@gmail.com>
2020-10-25 22:08:37 +0530

    added shell

--- regex/5_grouping_backreference.py
+++ regex/5_grouping_backreference.py
@@ -7,6 +7,8 @@ import re
 
 #Print the first occurrence of the repeating character. If there are no repeating characters, print -1.
 
+# backreference is used to match a pattern that was already matched. This is done using \1 which accesses the first matched string
+
 Pattern_1 = r'([a-zA-Z0-9])\1+'
 Test_String_1 = "..12345678910111213141516171820212223"
 
