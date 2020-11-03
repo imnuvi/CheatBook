f0612d5429eeb52c3d4dc4464dc5987b6873bd77
thedoodler <ramprakashsmail@gmail.com>
2020-11-03 16:35:31 +0530

    added printing fields

--- awk/1_printing.sh
+++ awk/1_printing.sh
@@ -3,3 +3,9 @@
 #the syntax is awk ' { command within braces } ' filename
 
 awk ' { print } ' usr_bin_data.txt
+
+
+# $ is used to access fields, $0 prints everything, $1 prints just the first element in each line.  useful for finding stuff from tables or when listing folders entirely
+
+
+awk ' { print $1 } ' usr_bin_data.txt
