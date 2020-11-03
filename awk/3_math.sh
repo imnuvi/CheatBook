2f5b29c3dee95ff2eed3b11f1c0ac7cfec05f24a
thedoodler <ramprakashsmail@gmail.com>
2020-11-03 16:52:28 +0530

    changed math

--- awk/3_math.sh
+++ awk/3_math.sh
@@ -1,5 +1,13 @@
 # awk can be used to perform math stuff on the values, for example in the given file we have all the sizes of binaries in bytes, so in order to show them in kilobytes we divide them by 1024
 
-# this displays all files with their sizes in kb
+# this displays all files with their sizes in kb along with "Kb"
 
-awk ' { print $1,$2/1024 } ' usr_bin_data.txt
+awk ' { print $1,$2/1024"Kb" } ' usr_bin_data.txt
+
+
+
+# math operators can be used on fields.
+
+# this prints all binaries with value greater than 20Kb
+
+awk ' $2 > 20000 { print $1,$2 } ' usr_bin_data.txt
