c811a65d42d019d14a683f1eb0b6ee5177aeea29
ram prakash <ramprakashsmail@gmail.com>
2021-02-12 00:48:36 +0530

    ADD:  tuples in basic collection files

---
+++ elixir/2_basic_collection_types.exs
@@ -0,0 +1,15 @@
+
+# Tuples
+
+my_tuple = { 1, :two, "three" }
+tuple2 = { :ok, 55, "go" }
+{ name, mark, prize } = { "ham", 50, :first }
+
+IO.puts(inspect my_tuple)
+IO.puts(inspect tuple2)
+IO.puts(name)
+IO.puts(mark)
+IO.puts(prize)
+
+
+# Lists
