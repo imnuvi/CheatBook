b68a639d237c2ac81619c89750cecaba150e4b93
ram prakash <ramprakashsmail@gmail.com>
2021-03-03 21:04:28 +0530

    ADD:  correct default function

--- elixir/Named_Functions/src_files/default_params.exs
+++ elixir/Named_Functions/src_files/default_params.exs
@@ -3,7 +3,17 @@ defmodule Default do
     IO.puts("YOU gave #{p1} #{p2} #{p3} #{p4}")
   end
 
-  def tests(p1, p2, p3) do
-    IO.puts("This test yields #{p1} #{p2} #{p3}")
+
+
+  def correct_func(p1, p2 \\ 33)
+
+  def correct_func(p1, p2) when is_number(p1) do
+    IO.puts("first one is a number #{p1} and #{p2} is maybe default")
   end
+
+  def correct_func(p1, p2) do
+    IO.puts("This yields #{p1} #{p2} ")
+  end
+
+
 end
