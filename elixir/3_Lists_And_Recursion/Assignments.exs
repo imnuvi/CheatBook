e3532479307476991f2b1acbc29ff876f0ad44a3
ram prakash <ramprakashsmail@gmail.com>
2021-03-13 18:12:01 +0530

    add getmax function

--- elixir/3_Lists_And_Recursion/Assignments.exs
+++ elixir/3_Lists_And_Recursion/Assignments.exs
@@ -1,10 +1,24 @@
 defmodule MyRecursion do
   # mapsum function that takes a list and a function, applies the function to every element in the list and sums the result
-
   def mapsum( list, func ), do: _mapsum( list, 0, func)
 
+  # privates
   defp _mapsum( [], value, _func ), do: value
   defp _mapsum( [ head | tail ], value, func ) do
     _mapsum(tail, value + func.(head), func)
+  end
+
+
+  # Max function that returns the maximum of a list
+
+  def getmax( list ), do: _getmax( list, 0 )
+
+  defp _getmax( [], value ), do: value
+  defp _getmax( [ head | tail ], value ) when (head > value) do
+    _getmax( tail, head )
+  end
+
+  defp _getmax( [ head | tail ], value ) when (head < value) do
+    _getmax( tail, value )
   end
 end
